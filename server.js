ebca8aef980711a210ce413660caed899c66a7d4
Groutch <hurizen1@hotmail.com>
2018-06-05 21:06:27 +0200

    add server log number of client connected

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 //on utilise express
-const express=require("express");
-const app =express();
+const express = require("express");
+const app = express();
 //on utilise le moteur de template ejs
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 //middleware: on indique que les fichiers statiques (css, stripts js...) sont dans le dossier public
 app.use(express.static("public"));
 //si un client accede à la racine, on affiche le fichier index.ejs
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 });
-server= app.listen(process.env.PORT || 8080);
+server = app.listen(process.env.PORT || 8080);
 //on utilise socket.io
 const io = require("socket.io")(server);
 //si un client se connecte...
 var nbclients = 0;
-io.on("connection", function(socket){
-    nbclients++;
-    console.log("utilisateurs connectés: "+nbclients);
+var allchatters=0;
+io.on("connection", function (socket) {
+    allchatters++;
+    console.log("connectés: " + allchatters);
     //Si le client nous envoie un message on le renvoie à tous les clients pour l'afficher
-    socket.on("message", function(data){
-       io.sockets.emit("message",{pseudo: data.pseudo, message : data.message});
+    socket.on("message", function (data) {
+        io.sockets.emit("message", {
+            pseudo: data.pseudo,
+            message: data.message
+        });
     });
-    
+    socket.on('disconnect', function () {
+        allchatters--;
+        console.log("cpnnectés: " + allchatters);
+    });
 });
